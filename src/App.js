5119a6787986a3bb483681eb7a2a4386ebcc616a
Binary44 <binarycoder.com@.com>
2022-10-26 11:12:54 +0100

    adding Tailwind

--- src/App.js
+++ src/App.js
@@ -3,7 +3,9 @@ import React from "react";
 function App() {
   return (
     <div className="App">
-      
+      <h1 className="text-3xl font-bold ">
+      Hello world!
+    </h1>
     </div>
   );
 }
